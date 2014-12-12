831c712521af25a193fa2c1eb6bace0ab9c94a74
David Greenspan <dgreenspan@alum.mit.edu>
2014-12-12 12:43:44 -0800

    Write eight queens as a unit test

--- api.js
+++ api.js
@@ -36,6 +36,10 @@ var VariableName = Match.Where(function (x) {
   check(x, String);
   return x.indexOf('\n') < 0;
 });
+var WholeNumber = Match.Where(function (x) {
+  check(x, Match.Integer);
+  return x >= 0;
+});
 
 PBSolver.prototype.addClause = function (positives, negatives) {
   negatives = negatives || [];
@@ -46,6 +50,57 @@ PBSolver.prototype.addClause = function (positives, negatives) {
     C._addClause(native.pushString(positives.join('\n')),
                  native.pushString(negatives.join('\n')));
   });
+};
+
+var TYPE_CODES = {
+  '<': -2, '<=': -1, '=': 0, '>=': 1, '>': 2
+};
+
+PBSolver.prototype.addConstraint = function (vars, coeffs, type, rhs) {
+  if (typeof coeffs === 'number') {
+    // turn a coeffs of `1` into `[1, 1, 1]`, for example, if there
+    // are three vars.
+    coeffs = _.map(vars, function () { return coeffs; });
+  }
+  coeffs.length = vars.length;
+  check(vars, [VariableName]);
+  check(coeffs, [WholeNumber]);
+  check(type, Match.OneOf('<', '<=', '=', '>=', '>'));
+  check(rhs, WholeNumber);
+
+  this._native.savingStack(function (native, C) {
+    var coeffsPtr = C.allocate(vars.length*4, 'i32', C.ALLOC_STACK);
+    _.each(coeffs, function (c, i) {
+      C.setValue(coeffsPtr + i*4, c, 'i32');
+    });
+    C._addConstraint(native.pushString(vars.join('\n')),
+                     coeffsPtr,
+                     TYPE_CODES[type],
+                     rhs);
+  });
+};
+
+PBSolver.prototype.exactlyOne = function (vars) {
+  // XXX consider improving translation to SAT using this paper:
+  // http://www.cs.cmu.edu/~wklieber/papers/2007_efficient-cnf-encoding-for-selecting-1.pdf
+  this.addConstraint(vars, 1, '=', 1);
+};
+
+PBSolver.prototype.atMostOne = function (vars) {
+  // consider optimizing this (see exactlyOne)
+  this.addConstraint(vars, 1, '<=', 1);
+};
+
+PBSolver.prototype.atLeastOne = function (vars) {
+  this.addClause(vars);
+};
+
+PBSolver.prototype.isTrue = function (v) {
+  this.addClause([v]);
+};
+
+PBSolver.prototype.isFalse = function (v) {
+  this.addClause([], [v]);
 };
 
 // returns `null` or an array of the variables that are positive,
--- pbsolver_tests.js
+++ pbsolver_tests.js
@@ -7,21 +7,107 @@ Tinytest.add("pbsolver - basic", function (test) {
   test.equal(solver._countLines("\n"), 0);
 });
 
-Tinytest.add("pbsolver - basic solve", function (test) {
+Tinytest.add("pbsolver - solve with clauses", function (test) {
   var solver = new PBSolver();
-  var exactlyOne = function (a, b, c) {
+  var exactlyOneOfTheseThree = function (a, b, c) {
     solver.addClause([a, b, c]); // at least one
     solver.addClause([], [a, b]); // not(A) or not(B)
     solver.addClause([], [b, c]); // etc
     solver.addClause([], [a, c]); // etc
   };
-  exactlyOne('11', '12', '13');
-  exactlyOne('21', '22', '23');
-  exactlyOne('31', '32', '33');
-  exactlyOne('11', '21', '31');
-  exactlyOne('12', '22', '32');
-  exactlyOne('13', '23', '33');
+  exactlyOneOfTheseThree('11', '12', '13');
+  exactlyOneOfTheseThree('21', '22', '23');
+  exactlyOneOfTheseThree('31', '32', '33');
+  exactlyOneOfTheseThree('11', '21', '31');
+  exactlyOneOfTheseThree('12', '22', '32');
+  exactlyOneOfTheseThree('13', '23', '33');
   solver.addClause(['12']);
   solver.addClause([], ['21']);
   test.equal(solver.solve(), ['12', '23', '31']);
+});
+
+Tinytest.add("pbsolver - solve with constraints", function (test) {
+  var solver = new PBSolver();
+  var exactlyOneOfTheseThree = function (a, b, c) {
+    solver.addConstraint([a, b, c], 1, '=', 1);
+  };
+  exactlyOneOfTheseThree('11', '12', '13');
+  exactlyOneOfTheseThree('21', '22', '23');
+  exactlyOneOfTheseThree('31', '32', '33');
+  exactlyOneOfTheseThree('11', '21', '31');
+  exactlyOneOfTheseThree('12', '22', '32');
+  exactlyOneOfTheseThree('13', '23', '33');
+  solver.addClause(['12']);
+  solver.addClause([], ['21']);
+  test.equal(solver.solve(), ['12', '23', '31']);
+});
+
+Tinytest.add("pbsolver - solve with exactlyOne", function (test) {
+  var solver = new PBSolver();
+  solver.exactlyOne(['11', '12', '13']);
+  solver.exactlyOne(['21', '22', '23']);
+  solver.exactlyOne(['31', '32', '33']);
+  solver.exactlyOne(['11', '21', '31']);
+  solver.exactlyOne(['12', '22', '32']);
+  solver.exactlyOne(['13', '23', '33']);
+  solver.isTrue('12');
+  solver.isFalse('21');
+  test.equal(solver.solve(), ['12', '23', '31']);
+});
+
+Tinytest.add("pbsolver - eight queens", function (test) {
+  var boardSquare = function (r, c) {
+    return String(r) + String(c);
+  };
+
+  var solver = new PBSolver();
+  var nums = _.range(1, 9); // 1..8
+  _.each(nums, function (x) {
+    // one per row x, one per column x
+    solver.exactlyOne(_.map(nums, function (y) {
+      return boardSquare(x, y);
+    }));
+    solver.exactlyOne(_.map(nums, function (y) {
+      return boardSquare(y, x);
+    }));
+  });
+
+  // At most one queen per diagonal.  A diagonal
+  // consists of squares whose row + column sums
+  // to a constant, or the horizontal flip of
+  // such a set of squares.
+  for (var flip = 0; flip <= 1; flip++) {
+    for (var sum = 2; sum <= 16; sum++) {
+      var vars = [];
+      for (var r = 1; r <= sum-1; r++) {
+        var c = sum - r;
+        if (flip)
+          c = 9-c;
+        if (r >= 1 && r <= 8 && c >= 1 && c <= 8) {
+          vars.push(boardSquare(r,c));
+        }
+      }
+      solver.atMostOne(vars);
+    }
+  }
+
+  var solution = solver.solve();
+
+  // solution might be, for example,
+  // ["16", "24", "31", "45", "58", "62", "77", "83"]
+  test.equal(solution.length, 8);
+  test.isTrue(/^([1-8][1-8],){7}[1-8][1-8]$/.test(solution.join(',')));
+  var assertEightDifferent = function (transformFunc) {
+    test.equal(_.uniq(_.map(solution, transformFunc)).length, 8);
+  };
+  // queens occur in eight different rows, eight different columns
+  assertEightDifferent(function (queen) { return queen.charAt(0); });
+  assertEightDifferent(function (queen) { return queen.charAt(1); });
+  // queens' row/col have eight different sums, eight different differences
+  assertEightDifferent(function (queen) {
+    return Number(queen.charAt(0)) - Number(queen.charAt(1));
+  });
+  assertEightDifferent(function (queen) {
+    return Number(queen.charAt(0)) + Number(queen.charAt(1));
+  });
 });
